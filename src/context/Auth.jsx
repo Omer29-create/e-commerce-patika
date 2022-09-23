db4428f99ee8900f6f0cfa7d8bf355c7b24da0f1
Ömer Aydin <64536242+Omer29-create@users.noreply.github.com>
2022-09-23 11:57:35 +0200

    Update Auth.jsx

--- src/context/Auth.jsx
+++ src/context/Auth.jsx
@@ -1,24 +1,55 @@
-import axios from 'axios';
-import React, { createContext, useEffect, useState } from 'react';
+import axios from "axios";
+import React, { createContext, useEffect, useRef, useState } from "react";
 
 export const Authen = createContext();
 
 export const AuthenProvider = (props) => {
+  const [load, setLoad] = useState([]);
+  const [adana, setAdana] = useState(true);
+  const [filter, setFilter] = useState([]);
+  const mailRef = useRef();
+  const passwordRef = useRef();
+  const nameRef = useRef();
+  const [reco, setReco] = useState({
+    name:'', email:'', password:''
+  });
 
-    const [load, setLoad] = useState('');
+  const tikla = (e) => {
+    e.preventDefault();
+    console.log(mailRef.current.value, passwordRef.current.value);
+  }
 
-    useEffect(() => {
-        axios.get('https://jsonplaceholder.typicode.com/users')
-        .then((res) => setLoad(res.data));
-        
-    }, []);
+  const tikla2 = (e) => {
+    e.preventDefault();
+    setReco([...reco, {
+      name:nameRef.current.value,
+      email:mailRef.current.value,
+      password:passwordRef.current.value
+    }])
+  }
 
-    console.log('load', load);
+  useEffect(() => {
+    return () => {
+      axios
+        .get("https://jsonplaceholder.typicode.com/users")
+        .then((res) => setLoad(res.data).limit(5));
+    };
+  }, []);
 
+  console.log("filter", filter);
 
-    return(
-        <Authen.Provider value={{load:load}}>
-            {props.children}
-        </Authen.Provider>
-    )
-}
+  useEffect(() => {
+    const ada = load.filter((er) =>
+      er.name.toLowerCase().includes(filter.toLowerCase())
+    );
+    if (ada.length > 0) {
+      setLoad(ada);
+    } 
+  }, [filter]);
+
+  return (
+    <Authen.Provider value={{ load, adana, setAdana, filter, setFilter, mailRef, passwordRef, tikla, nameRef, tikla2 }}>
+      {props.children}
+    </Authen.Provider>
+  );
+};
